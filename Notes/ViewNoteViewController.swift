ef3687e8b4dded25f1c39f6093d9ab019af13c8b
Gayatri Patil <gsp1294@gmail.com>
2018-03-23 22:37:35 +0530

    complete initial UI

---
+++ Notes/ViewNoteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewNoteViewController.swift
+//  Notes
+//
+//  Created by Gayatri Patil on 23/03/18.
+//  Copyright © 2018 Gayatri Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
